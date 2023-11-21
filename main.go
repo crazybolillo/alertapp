35f8f057804df4b68897d7c8a6328ff76da36314
CrazyBolillo <antonio@zoftko.com>
2023-11-21 17:13:26 -0600

    feat: use in memory storage
    The concept of what an Alert is and a storage interface for it has been
    enabled. This adds memory to the application and enables the usage of a
    database down the line.

--- main.go
+++ main.go
@@ -1,16 +1,112 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	_, _ = io.WriteString(w, "Hello World")
+const (
+	defaultPage          = 0
+	defaultSize          = 20
+	AlertCountHttpHeader = "X-Alertapp-Count"
+)
+
+type Alert struct {
+	UUID string     `json:",omitempty"`
+	Time *time.Time `json:",omitempty"`
+	Info string
+}
+
+type AlertStore interface {
+	Retrieve(page int, size int) []Alert
+	Store(alert Alert) Alert
+}
+
+type InMemoryAlertStore struct {
+	Storage []Alert
+}
+
+func (store *InMemoryAlertStore) Retrieve(page int, size int) []Alert {
+	offset := page * size
+	if offset > len(store.Storage) {
+		return []Alert{}
+	}
+
+	size = offset + size
+	if size >= len(store.Storage) {
+		size = len(store.Storage)
+	}
+
+	return store.Storage[offset:size]
+}
+
+func (store *InMemoryAlertStore) Store(alert Alert) Alert {
+	store.Storage = append(store.Storage, alert)
+	return store.Storage[len(store.Storage)-1]
+}
+
+func indexGet(store AlertStore, w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	page, err := strconv.Atoi(params.Get("page"))
+	if err != nil {
+		page = defaultPage
+	}
+	size, err := strconv.Atoi(params.Get("size"))
+	if err != nil {
+		size = defaultSize
+	}
+	if page < 0 || size < 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	alerts := store.Retrieve(page, size)
+	w.Header().Add(AlertCountHttpHeader, strconv.Itoa(len(alerts)))
+	for _, alert := range alerts {
+		_, _ = fmt.Fprintf(w, "UUID: %s --- %s --- %s\n", alert.UUID, alert.Time, alert.Info)
+	}
+}
+
+func indexPost(store AlertStore, w http.ResponseWriter, r *http.Request) {
+	var alert Alert
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(&alert); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now()
+	alert.UUID = uuid.NewString()
+	alert.Time = &now
+
+	store.Store(alert)
+}
+
+func indexHandler(store AlertStore) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		switch r.Method {
+		case http.MethodGet:
+			indexGet(store, w, r)
+		case http.MethodPost:
+			indexPost(store, w, r)
+		}
+	}
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
+	storage := InMemoryAlertStore{}
+
+	http.HandleFunc("/", indexHandler(&storage))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
